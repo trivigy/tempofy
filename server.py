53390d3f5a1d3de8f2b81b0fe66d013be0833540
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-17 15:40:51 -0500

    adding json incoming data processing.

--- server.py
+++ server.py
@@ -5,11 +5,18 @@ from tempofy import get_song
 app = Flask(__name__)
 
 
-@app.route('/song', methods=['GET'])
+@app.route('/', methods=['POST'])
 def song():
-    if request.method == 'GET':
-        song_id = get_song(130)
-        return Response(song_id, status=200)
+    if request.method == 'POST':
+        if request.headers['Content-Type'] == 'application/json':
+            if len(request.data) != 0:
+                data = json.loads(request.data)
+                print data['key1']
+            else:
+                return Response(status = 400)
+        # song_id = get_song(130)
+        # return Response(song_id, status=200)
+        return Response(json.dumps({"data":"WORKS"}), status=200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
