f711f7aa2b5673874bde4b3ca53b49b2abf6fde7
Gene Gurvich <genegurvich@gmail.com>
2015-01-17 15:37:14 -0500

    Refactor tempofy, add /song endpoint

--- server.py
+++ server.py
@@ -1,20 +1,15 @@
-#!/usr/bin/python
+#!/private/var/local/venv/tempofy/bin/python
 from flask import Flask, request, Response
-
+from tempofy import get_song
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def index():
-    if request.method == 'POST':
-        print len(request.data)
-        # if len(request.data) != 0:
-        #     return Response(status = 201)
-        #     # data = request.data
-        # else:
-        #     return Response(status = 400)
 
-        return Response("BLAH\n", status = 200)
+@app.route('/song', methods=['GET'])
+def song():
+    if request.method == 'GET':
+        song_id = get_song(130)
+        return Response(song_id, status=200)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
