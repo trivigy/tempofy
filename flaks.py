35b611f12e4377d7591c69f33ad5f0c92e1893d5
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-17 12:28:15 -0500

    removes testfile and adds the flask server file.

---
+++ flaks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        print len(request.data)
+        # if len(request.data) != 0:
+        #     return Response(status = 201)
+        #     # data = request.data
+        # else:
+        #     return Response(status = 400)
+
+        return Response("BLAH\n", status = 200)
+
+if __name__ == '__main__':
+    app.run(debug = True)
