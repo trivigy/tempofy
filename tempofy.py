a7870f0b70f5c7a58cd6098ff58ea7fe57b19803
Gene Gurvich <genegurvich@gmail.com>
2015-01-18 02:41:17 -0500

    Update tempofy

--- tempofy.py
+++ tempofy.py
@@ -2,23 +2,28 @@
 import urllib2
 import json
 import random
+import time
 
 
-def get_song(tempo=124):
+def spotify(tempo=124):
     api_key = '6AR8ZBJDQ5QTT0KK0'
     response_format = 'json'
     mintempo = tempo - 1
     maxtempo = tempo + 1
-    request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&results=100&style=edm&rank_type=familiarity&sort=tempo-asc&bucket=id:spotify&bucket=tracks' % \
-        (api_key, response_format, str(mintempo), str(maxtempo))
-    try:
-        response = urllib2.urlopen(request).read()
-    except (urllib2.URLError, urllib2.HTTPError):
-        return None
+    request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&results=100&style=edm&rank_type=familiarity&sort=tempo-asc&bucket=id:spotify&bucket=tracks' % (api_key, response_format, str(mintempo), str(maxtempo))
+    response = urllib2.urlopen(request).read()
     parsed_response = json.loads(response)
     songs = parsed_response['response']['songs']
+    song = songs[0]
     song = random.choice(songs)
-    spotify_id = song['tracks'][0]['foreign_id'].split(':')[-1]
+    while True:
+        try:
+            spotify_id = song['tracks'][0]['foreign_id'].split(':')[-1]
+            break
+        except:
+            pass
     return spotify_id
 
-get_song()
+while True:
+    time.sleep(2)
+    print spotify((random.random() * 30) + 100)
