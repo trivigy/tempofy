75ec765ea38476078005e861f4021893ff3c3245
Gene Gurvich <genegurvich@gmail.com>
2015-01-17 20:45:29 -0500

    Update tempofy

--- tempofy.py
+++ tempofy.py
@@ -5,8 +5,8 @@ import json
 def get_song(self, tempo=120):
     api_key = '6AR8ZBJDQ5QTT0KK0'
     response_format = 'json'
-    mintempo = tempo
-    maxtempo = tempo
+    mintempo = tempo - 5
+    maxtempo = tempo + 5
     request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&bucket=id:spotify&bucket=tracks' % \
         (api_key, response_format, str(mintempo), str(maxtempo))
     try:
