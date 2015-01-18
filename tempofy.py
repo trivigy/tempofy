ddc0e1cb996bd9673d2c11693bec1adf3edede5e
konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-18 21:15:42 +0000

    during presentation updates that optimize the spotify search.

--- tempofy.py
+++ tempofy.py
@@ -7,19 +7,23 @@ import time
 def spotify(tempo=124):
     api_key = '6AR8ZBJDQ5QTT0KK0'
     response_format = 'json'
-    style = 'drumnbass'
+    style = 'drumandbass'
     rank_type = 'familiarity'
     sort = 'tempo-asc'
-    if tempo > 200: tempo = 200
-    mintempo = int(tempo - 1)
-    maxtempo = int(tempo + 1)
+    if tempo > 400: tempo = 400
+    mintempo = int(tempo - 5)
+    maxtempo = int(tempo + 0)
     request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&results=100&style=%s&rank_type=%s&sort=%s&bucket=id:spotify&bucket=tracks' % (api_key, response_format, str(mintempo), str(maxtempo), style, rank_type, sort)
 
     response = urllib2.urlopen(request).read()
     parsed_response = json.loads(response)
     songs = parsed_response['response']['songs']
-    
-    i = int(random.random()) * len(songs)
+    print "#songs: ", len(songs)
+    if len(songs) > 2:
+        i = random.randint(0, len(songs) - 1)
+    else:
+        i = 0
+    print "ith song: ", i
     try: 
         song = songs[i]
         spotify_id = song['tracks'][0]['foreign_id'].split(':')[-1]
