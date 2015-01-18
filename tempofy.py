4fd81d91dc9f64dcc1107767d3c5a9c086ddc06b
Jessica Zhao <jessicamtzhao@gmail.com>
2015-01-18 05:58:25 -0500

    Tokenize request URL; add style field (edm, pop, etc.)

--- tempofy.py
+++ tempofy.py
@@ -7,10 +7,13 @@ import time
 def spotify(tempo=124):
     api_key = '6AR8ZBJDQ5QTT0KK0'
     response_format = 'json'
+    style = 'pop'
+    rank_type = 'familiarity'
+    sort = 'tempo-asc'
     if tempo > 200: tempo = 200
     mintempo = int(tempo - 1)
     maxtempo = int(tempo + 1)
-    request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&results=100&style=edm&rank_type=familiarity&sort=tempo-asc&bucket=id:spotify&bucket=tracks' % (api_key, response_format, str(mintempo), str(maxtempo))
+    request = 'http://developer.echonest.com/api/v4/song/search?api_key=%s&format=%s&min_tempo=%s&max_tempo=%s&results=100&style=%s&rank_type=%s&sort=%s&bucket=id:spotify&bucket=tracks' % (api_key, response_format, str(mintempo), str(maxtempo), style, rank_type, sort)
 
     response = urllib2.urlopen(request).read()
     parsed_response = json.loads(response)
